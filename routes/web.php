393e33f23c6f02ad7cfc2a94011ae2733fc22975
SlanSaf <slansaf@yandex.ru>
2026-01-05 16:08:55 +0300

    Add posts controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\PostsController;
 
 Route::get('/', function () {
     return view('home');
@@ -14,3 +15,5 @@ Route::get('/works', function () {
 Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.form');
 Route::get('/contact/all', [ContactController::class, 'allData'])->name('contact.all');
 Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
+
+Route::resource('posts', PostsController::class);
